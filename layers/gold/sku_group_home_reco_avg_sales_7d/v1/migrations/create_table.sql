4a5cff8eb7667a9fc94b5e28feb41cf5bcbee3ff
Shirokov Sergey <Dreammer2012@yandex.ru>
2026-06-08 16:30:16 +0300

    test: migration

--- layers/gold/sku_group_home_reco_avg_sales_7d/v1/migrations/create_table.sql
+++ layers/gold/sku_group_home_reco_avg_sales_7d/v1/migrations/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS {target_table} (
     date DATE COMMENT 'Дата партиционирования, соответствует ds расчета',
-    sku_group_id BIGINT COMMENT 'ID sku group',
+    sku_group_id BIINT COMMENT 'ID sku group',
     home_reco_avg_sales_count_7d DOUBLE COMMENT 'Среднее дневное количество завершенных товарных позиций из рекомендаций с главной за окно [ds - 7, ds - 1]'
 )
 USING iceberg
