db568d7fc15405fb2acebfad94a1f122ba99cd03
Shirokov Sergey <Dreammer2012@yandex.ru>
2026-06-30 08:51:31 +0300

    feat: use spark

---
+++ layers/gold/calculated_at_sku_group_id_promotion_id/dynamic_pricing_sku_group_price_features/v1/job/arguments.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+from job.entities import Arguments
+
+
+def parse_arguments() -> Arguments:
+    parser = ArgumentParser()
+    parser.add_argument("--partition_start", type=str, required=True)
+    parser.add_argument("--partition_end", type=str, required=True)
+    parser.add_argument("--table_name", type=str, required=True)
+
+    namespace, _ = parser.parse_known_args()
+    return Arguments(
+        partition_start=namespace.partition_start,
+        partition_end=namespace.partition_end,
+        table_name=namespace.table_name,
+    )
